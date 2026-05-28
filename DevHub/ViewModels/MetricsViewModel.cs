00b864373a04fad1905860b3391c58693da70fc9
mramirezg <mramirezg@jumapacelaya.gob.mx>
2026-05-28 16:00:38 -0600

    En el modulo de metricas del sistema ya obtiene la informacion real del sistema y no ficticia como al principio

--- DevHub/ViewModels/MetricsViewModel.cs
+++ DevHub/ViewModels/MetricsViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,7 +14,8 @@ namespace DevHub.ViewModels
     public partial class MetricsViewModel : ObservableObject
     {
         private readonly DispatcherTimer _timer;
-        private readonly Process _currentProcess;
+        private ManagementObjectSearcher? _cpuSearcher;
+        private double _totalRamGb;
 
         [ObservableProperty]
         private double _cpuUsage;
@@ -26,7 +28,7 @@ namespace DevHub.ViewModels
 
         public MetricsViewModel()
         {
-            _currentProcess = Process.GetCurrentProcess();
+            InicializarMetricasSistema();
 
             _timer = new DispatcherTimer
             {
@@ -35,20 +37,76 @@ namespace DevHub.ViewModels
             _timer.Tick += Timer_Tick;
         }
 
+        private void InicializarMetricasSistema()
+        {
+            try
+            {
+                // Se inicializa el buscador de WMI para la carga de CPU
+                _cpuSearcher = new ManagementObjectSearcher("select LoadPercentage from Win32_Processor");
+
+                // Obtenemos la memoria RAM total física del equipo (en bytes) y la convertimos a GB
+                var ramSearcher = new ManagementObjectSearcher("select TotalVisibleMemorySize from Win32_OperatingSystem");
+                foreach (var obj in ramSearcher.Get())
+                {
+                    double totalRamKb = Convert.ToDouble(obj["TotalVisibleMemorySize"]);
+                    _totalRamGb = totalRamKb / (1024 * 1024.0);
+                    break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error al inicializar componentes de hardware: {ex.Message}");
+                _totalRamGb = 16.0;
+            }
+        }
+
         public void StartMonitoring() => _timer.Start();
         public void StopMonitoring() => _timer.Stop();
 
         private void Timer_Tick(object? sender, object e)
         {
-            _currentProcess.Refresh();
+            double cpuCargaActual = 0;
+            try
+            {
+                if (_cpuSearcher != null)
+                {
+                    foreach (var obj in _cpuSearcher.Get())
+                    {
+                        var valorCpu = obj["LoadPercentage"];
+                        if (valorCpu != null)
+                        {
+                            cpuCargaActual = Convert.ToDouble(valorCpu, System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine($"Fallo al leer CPU: {ex.Message}");
+            }
+            CpuUsage = cpuCargaActual;
 
-            double allocatedRam = _currentProcess.PrivateMemorySize64 / (1024.0 * 1024.0);
+            try
+            {
+                var osSearcher = new ManagementObjectSearcher("select FreePhysicalMemory from Win32_OperatingSystem");
+                foreach (var obj  in osSearcher.Get())
+                {
+                    double freeRamKb = Convert.ToDouble(obj["FreePhysicalMemory"]);
+                    double freeRamGb = freeRamKb / (1024.0 * 1024.0);
 
-            Random rand = new();
-            _cpuUsage = rand.Next(8, 24);
+                    double usedRamGb = _totalRamGb - freeRamGb;
 
-            RamPercentage = Math.Min(100, (allocatedRam / 800) * 100);
-            RamUsageStr = $"{allocatedRam:F1} MB usados por DevHub";
+                    RamPercentage = (usedRamGb / _totalRamGb) * 100;
+
+                    RamUsageStr = $"{usedRamGb:F1} GB usados de {_totalRamGb:F0} GB";
+                    break;
+                }
+            }
+            catch
+            {
+                RamUsageStr = "Error al leer memoria";
+            }
         }
     }
 }
