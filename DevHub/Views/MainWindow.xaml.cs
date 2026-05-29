db1b22a739fd8d89161a4635b3d07ce98d7175fe
mramirezg <mramirezg@jumapacelaya.gob.mx>
2026-05-29 15:46:08 -0600

    Se agregaron validaciones para que la app se conporte bien si se cambia del tema claro al oscuro y viceversa

--- DevHub/Views/MainWindow.xaml.cs
+++ DevHub/Views/MainWindow.xaml.cs
@@ -12,23 +12,72 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.UI;
+using WinRT.Interop;
+using Microsoft.UI.Windowing;
+using Windows.UI;
 
 namespace DevHub.Views
 {
     public sealed partial class MainWindow : Window
     {
+        private AppWindow _appWindow;
+
         public MainWindow()
         {
             this.InitializeComponent();
 
-            MainNavView.SelectedItem = MainNavView.MenuItems[0];
-            ContentFrame.Navigate(typeof(GitHubPage));
+            ExtendsContentIntoTitleBar = true;
+
+            IntPtr windowHandle = WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            _appWindow = AppWindow.GetFromWindowId(windowId);
+
+            ActualizarColoresBarraTitulo();
+
+            if (Content is FrameworkElement rootElement)
+            {
+                rootElement.ActualThemeChanged += RootElement_ActualThemeChanged;
+            }
+        }
+
+        private void RootElement_ActualThemeChanged(FrameworkElement sender, object args)
+        {
+            ActualizarColoresBarraTitulo();
+        }
+
+        private void ActualizarColoresBarraTitulo()
+        {
+            if (_appWindow == null) return;
+
+            _appWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            _appWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            var temaActual = ElementTheme.Default;
+            if (Content is FrameworkElement rootElement)
+            {
+                temaActual = rootElement.ActualTheme;
+            }
+
+            if (temaActual == ElementTheme.Dark)
+            {
+                _appWindow.TitleBar.ButtonForegroundColor = Colors.White;
+                _appWindow.TitleBar.ButtonHoverForegroundColor = Colors.White;
+                _appWindow.TitleBar.ButtonHoverBackgroundColor = Color.FromArgb(30, 255, 255, 255);
+            }
+            else
+            {
+                _appWindow.TitleBar.ButtonForegroundColor = Colors.Black;
+                _appWindow.TitleBar.ButtonHoverForegroundColor = Colors.Black;
+                _appWindow.TitleBar.ButtonHoverBackgroundColor = Color.FromArgb(30, 0, 0, 0);
+            }
         }
 
         private void MainNavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             if (args.IsSettingsSelected)
             {
+                ContentFrame.Navigate(typeof(SettingsPage));
                 return;
             }
 
@@ -46,6 +95,9 @@ namespace DevHub.Views
                         break;
                     case "metrics":
                         ContentFrame.Navigate(typeof(MetricsPage));
+                        break;
+                    case "settings":
+                        ContentFrame.Navigate(typeof(SettingsPage));
                         break;
                 }
             }
